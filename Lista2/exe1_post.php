390c03885c042b230fe6fa713a44cf5395146ca9
Daniel-Americo <daniel-americo@hotmail.com>
2025-02-23 09:46:26 -0300

    Atualizando nomenclatura dos exe

--- Lista2/exe1_post.php
+++ Lista2/exe1_post.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-  <form method="post" action="respostaex1.php">
+  <form method="post" action="exe1_resposta.php">
                             
     <div class="mb-3">
          <label for="valor1" class="form-label">Insira o primeiro numero</label>                       
