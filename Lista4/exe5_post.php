3f7a23fc99c70d8ff39a40545e6424f975f2d070
Daniel-Americo <daniel-americo@hotmail.com>
2025-03-11 10:55:32 -0300

    exe5 Lista 4

---
+++ Lista4/exe5_post.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>lista 4 </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+  <form method="post" action = "exe5_resposta.php">
+                        
+    <div class="mb-3">
+        <label for="valor" class="form-label">Insira o Valor e receberá a raiz quadrada dele: </label>
+        <input type="number" id="valor" name="valor" class="form-control">
+    </div>
+
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
