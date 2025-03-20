d7847c63c80cd6908b76c23d70bfb417eecb1657
Daniel-Americo <daniel-americo@hotmail.com>
2025-03-20 17:42:28 -0300

    exe 5 Lista 5

---
+++ Lista5/exe5_post.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>lita 5 </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <form action="exe5_resposta.php" method="post">
+    <?php for($i = 0; $i < 5; $i++): ?> 
+        <input type="text" name="titulo[]" placeholder="Titulo"/>
+        <input type="float" name="quant[]" placeholder="Quantidade"/>
+        <br/>
+        <?php endfor; ?>
+    <button type="submit">Enviar</button>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Lista5/exe5_resposta.php
@@ -0,0 +1,42 @@
+<?php 
+    declare(strict_types=1);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista 5 </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php 
+
+    if($_SERVER["REQUEST_METHOD"] == 'POST'){
+        $estoque = array();
+        try {
+            for($i = 0; $i < count($_POST['titulo']); $i ++){
+                $titulo = $_POST['titulo'][$i];
+                $quant = (float)$_POST['quant'][$i];
+                if($quant < 5){
+                    echo "Alerta o livro:<strong> $titulo</strong>, Atingiu a Quantidade minima no estoque atingida<br>";
+                    continue;
+                }
+                $estoque[$titulo]= $quant;
+            }
+            asort($estoque);
+
+            foreach($estoque as $titulo => $quant){
+                echo "Titulo do livro: $titulo : Quantidade em estoque: $quant <br>";
+            }
+    
+        }catch(Exception $e){
+            echo $e->getMessage();}
+
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
