04c3bc38a36059e065d1a61ca79119404bf2223e
Daniel-Americo <daniel-americo@hotmail.com>
2025-03-27 13:25:23 -0300

    condição pro valor da nota

--- Lista5/exe2_resposta.php
+++ Lista5/exe2_resposta.php
@@ -20,6 +20,11 @@
                 $nota1 = $_POST['nota1'][$i];
                 $nota2 = $_POST['nota2'][$i];
                 $nota3 = $_POST['nota3'][$i];
+                
+                if (($nota1 < 0 || $nota1 > 10) || ($nota2 < 0 || $nota2 > 10) || ($nota3 < 0 || $nota3 > 10)) {
+                  echo "</br>Nota do Aluno $nome, deve ser entre 0 e 10.";
+                  continue;
+                }
                 $media = ($nota1 + $nota2 + $nota3) / 3;
                 $medias[$nome] = $media;
             }
