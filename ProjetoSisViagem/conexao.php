2bf5b7a720d8bd225e5f1ad4344147628475b3a9
Daniel-Americo <daniel-americo@hotmail.com>
2025-04-24 20:59:47 -0300

    Update conexao.php

--- ProjetoSisViagem/conexao.php
+++ ProjetoSisViagem/conexao.php
@@ -2,7 +2,7 @@
 
 declare(strict_types =1);
 
-$dominio = 'mysql:host=localhost;dbname=bancophp'; //dbname é o nome do arquivo de banco de dados.
+$dominio = 'mysql:host=localhost;dbname=projetophp'; //dbname é o nome do arquivo de banco de dados.
 $usuario = 'root'; //nome do usuario padrao do bd é root.
 $senha = ''; //senha do banco de dados se tiver.
 
