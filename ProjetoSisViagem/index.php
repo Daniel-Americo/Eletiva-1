66ac78adcc17fb8b6ba05afa34baf7b7fbd50302
Daniel-Americo <daniel-americo@hotmail.com>
2025-04-26 11:05:01 -0300

    att projeto

--- ProjetoSisViagem/index.php
+++ ProjetoSisViagem/index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container">
-    <h1 class="mt-5">Sistema de Estoque</h1>
+    <h1 class="mt-5">Sistema de Pacotes de Viagens</h1>
 
     <?php
         require_once('conexao.php');
